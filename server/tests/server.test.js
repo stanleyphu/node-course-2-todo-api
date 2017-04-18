d4f937966a05de670b309350f9608178d57387aa
Stanley Phu <sphu@ucsd.edu>
2017-04-17 23:53:52 -0700

    Add tests for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -249,7 +249,7 @@ describe('POST /users', () => {
   });
 });
 
-describe('POST /users/login', (done) => {
+describe('POST /users/login', () => {
   it('should login user and return auth token', (done) => {
     request(app)
       .post('/users/login')
@@ -294,6 +294,25 @@ describe('POST /users/login', (done) => {
         }
 
         User.findById(users[1]._id).then((user) => {
+          expect(user.tokens.length).toBe(0);
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+});
+
+describe('DELETE /users/me/token', () => {
+  it('should remove auth token on logout', (done) => {
+    request(app)
+      .delete('/users/me/token')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(users[0]._id).then((user) => {
           expect(user.tokens.length).toBe(0);
           done();
         }).catch((e) => done(e));
